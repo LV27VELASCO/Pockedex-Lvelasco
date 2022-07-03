1938550ca4284997a5974a8ca95256eebfcbe8f9
Luis Velasco <luiselsw27@gmail.com>
2022-07-03 13:30:21 -0500

    abreviatura

--- src/elements/Pokedex/PockeCard.jsx
+++ src/elements/Pokedex/PockeCard.jsx
@@ -4,9 +4,9 @@ import usePokemons from '../../Hooks/usePokemons'
 
 const PockeCard = ({ pokemons }) => {
 
-    const { pockemon,loading } = usePokemons({ pokemons })
+    const { pockemon, loading } = usePokemons({ pokemons })
     const { backgroundColor } = useCardBackground({ pockemon })
-    
+
 
 
     return (
@@ -26,34 +26,34 @@ const PockeCard = ({ pokemons }) => {
                     </header>
                     <div className='caracteristicas'>
                         <div className='poder'>
-                            <h4>{pockemon?.stats[0].stat.name}</h4>
+                            <h4>HP</h4>
                             <span>{pockemon?.stats[0].base_stat}</span>
                         </div>
                         <div className='poder'>
-                            <h4>{pockemon?.stats[5].stat.name}</h4>
-                            <span>{pockemon?.stats[5].base_stat}</span>
-                        </div>
-                        <div className='poder'>
-                            <h4>{pockemon?.stats[2].stat.name}</h4>
-                            <span>{pockemon?.stats[2].base_stat}</span>
-                        </div>
-                        <div className='poder'>
-                            <h4>{pockemon?.stats[1].stat.name}</h4>
+                            <h4>ATQ</h4>
                             <span>{pockemon?.stats[1].base_stat}</span>
                         </div>
                         <div className='poder'>
-                            <h4>{pockemon?.stats[4].stat.name}</h4>
+                            <h4>DEF</h4>
+                            <span>{pockemon?.stats[2].base_stat}</span>
+                        </div>
+                        <div className='poder'>
+                            <h4>SPD</h4>
+                            <span>{pockemon?.stats[5].base_stat}</span>
+                        </div>
+                        <div className='poder'>
+                            <h4>STK</h4>
                             <span>{pockemon?.stats[4].base_stat}</span>
                         </div>
                         <div className='poder'>
-                            <h4>{pockemon?.stats[3].stat.name}</h4>
+                            <h4>SDF</h4>
                             <span>{pockemon?.stats[4].base_stat}</span>
                         </div>
                     </div>
                 </section>
 
                 <div className='loader'>
-                    <div className={`loaderLeft ${loading?"":"animate"}`}>
+                    <div className={`loaderLeft ${loading ? "" : "animate"}`}>
                         <div className="loaderBlock-Red"></div>
                         <div className="loaderBlock-Black">
                             <div className="linea1"></div>
@@ -66,7 +66,7 @@ const PockeCard = ({ pokemons }) => {
                             </div>
                         </div>
                     </div>
-                    <div className={`loaderRight ${loading?'':"animate"}`}>
+                    <div className={`loaderRight ${loading ? '' : "animate"}`}>
                         <div className="loaderBlock-Black">
                             <div className="circle-Rigth">
                                 <div className="circle-White">
